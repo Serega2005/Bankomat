638838e75255e57f96aba1fd009433be4bdded47
Serega <Schuganiserega@mail.ru>
2021-06-23 17:17:13 +0400

    Bankomat DONE

---
+++ ConsoleApp5/Account.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class Account
+    {
+        public Message Info;
+        public Message Error;
+
+        private string Name;
+        private string LastName;
+
+        public int Sum { get; set; } = 0;
+
+        private bool bank_account = false;
+        private double Bablo;
+
+        public void Open(string Name, string LastName)
+        {
+            if (bank_account)
+            {
+                this.Name = Name;
+                this.LastName = LastName;
+                bank_account = true;
+                Info?.Invoke("Счёт на имя: '" + Name + " " + LastName + "' Открыт");
+            }
+            else
+            {
+                Error?.Invoke("У вас уже есть открытый счёт");
+            }
+        }
+
+        public void SetBablo()
+        {
+            if (bank_account == true)
+            {
+                Info?.Invoke("У вас на счету " + Sum + " денег");
+                Info?.Invoke("Сколько желаете положить? ");
+                int IncludedBablo = Convert.ToInt32(Console.ReadLine());
+                if (IncludedBablo < 0)
+                {
+                    Error?.Invoke("Мы не можем положить на счёт отрицательное количество денег");
+                }
+                Info?.Invoke("Теперь у вас на счету" + (Sum + IncludedBablo) + "денег");
+            }
+            else
+            {
+                Error?.Invoke("У вас нет счёта, чтобы положить деньги");
+            }
+        }
+
+        public void TakeBablo()
+        {
+            if (bank_account == true)
+            {
+                Info?.Invoke("У вас на счету " + Sum + " денег");
+                Info?.Invoke("Сколько желаете снять?");
+                int TakenBablo = Convert.ToInt32(Console.ReadLine());
+                if (TakenBablo > Sum)
+                {
+                    Error?.Invoke("У вас нет достаточного количества денег на счету, чтобы выполнить данную операцию");
+                }
+                if (TakenBablo < 0)
+                {
+                    Error?.Invoke("Мы не можем снять с счёта отрицательно количество денег");
+                }
+                else
+                {
+                    Info?.Invoke("Теперь у вас на счету" + (Sum - TakenBablo) + "денег");
+                }
+            }
+            else
+            {
+                Error?.Invoke("Вы не можете снять деньги поскольку у вас нет открытого счёта");
+            }
+        }
+
+        public void Close()
+        {
+            if (bank_account == true)
+            {
+                bank_account = false;
+                Info?.Invoke("Вы закрыли счёт");
+            }
+            else
+            {
+                Error?.Invoke("У вас нет открытого счёта");
+            }
+        }
+
+    }
+}
+// ДОМАШКА
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bank
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Мы приветствуем вас в Сбербанке, какую операцию с вашим счётом вы хотите сделать?");
+
+            Account account = new Account();
+            account.Info = ShowInfo;
+            account.Error = ShowError;
+
+            string Name = "";
+            string LastName = "";
+
+            for (; ; )
+            {
+                int cislo = 0;
+
+                Console.WriteLine("1. Открыть счёт", cislo);
+                Console.WriteLine("2. Внести деньги", cislo);
+                Console.WriteLine("3. Снять деньги", cislo);
+                Console.WriteLine("4. Закрыть счёт", cislo);
+                Console.WriteLine("Введите имя - ", Name);
+                Console.WriteLine("Введите фамилию - ", LastName);
+                if (cislo == 1)account.Open(Name, LastName);
+                if (cislo == 2)account.SetBablo();
+                if (cislo == 3)account.TakeBablo();
+                if (cislo == 4)account.Close();
+            }
+
+            static void ShowInfo(string message)
+            {
+                Console.WriteLine(message);
+            }
+            static void ShowError(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
